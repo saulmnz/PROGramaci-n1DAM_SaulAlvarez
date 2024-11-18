153b392bff477cc74ae79e196409e49b6eaf86ea
Saul <salvarezmonzon@danielcastelao.org>
2024-11-18 10:59:48 +0100

    Segundo

---
+++ BOLETIN2/src/Ejercicio2.java
@@ -0,0 +1,17 @@
+/**
+ * Boletín 2 Ejercicio 2; Programa que calcule el area de un cuadrado
+ * @version 1.1
+ * @author SAUL ALVAREZ MONZON
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        int lado = 3;
+        int area = lado * lado;
+
+        System.out.println("El area del cuadrado de lado 3 es = "+area);
+
+
+    }
+
+}
