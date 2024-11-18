b9f8042a1784a02badd387bc088525b750e20afc
Saul <salvarezmonzon@danielcastelao.org>
2024-11-18 10:54:33 +0100

    Primeros Ejercicios

---
+++ BOLETIN2/src/Ejercicio1.java
@@ -0,0 +1,17 @@
+/**
+ * Boletín 2 Ejercicio 1; Programa que calcule el area de un triángulo
+ * @version 1.1
+ * @author SAUL ALVAREZ MONZON
+ */
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        int base = 4 ;
+        int altura = 3 ;
+        int area = base * altura ;
+
+        System.out.println("Área del Triángulo ="+area);
+
+    }
+}
